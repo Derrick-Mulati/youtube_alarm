fd1af58dfc04283c149e1b6bf23af4d9dd466d73
Mullah-X <mulatiderrick@gmail.com>
2024-08-29 07:01:50 +0300

    Perfecto

---
+++ youtube.py
@@ -0,0 +1,25 @@
+import webbrowser
+import time
+from datetime import datetime
+
+# Set the time you want to wake up (8 AM)
+wake_up_time = "07:01"
+
+# URL of the YouTube video you want to play
+video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # Replace with your desired video URL
+
+def wait_until(target_time):
+    while True:
+        now = datetime.now().strftime("%H:%M")
+        if now == target_time:
+            break
+        time.sleep(30)  # Check every 30 seconds
+
+def play_video(url):
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    print(f"Waiting until {wake_up_time}...")
+    wait_until(wake_up_time)
+    print("It's time! Opening YouTube video...")
+    play_video(video_url)
